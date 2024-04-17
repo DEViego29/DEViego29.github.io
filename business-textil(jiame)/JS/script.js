7848870a779e8738f80a6e9cdf94bdb0bcd5eda9
DEViego29 <163369038+DEViego29@users.noreply.github.com>
2024-04-16 21:24:57 -0500

    Add files via upload

---
+++ business-textil(jiame)/JS/script.js
@@ -0,0 +1,70 @@
+const barra = document.querySelector(".sidebar");
+
+      function mostrarBarra() {
+        barra.style.display = "flex";
+      }
+
+      function ocultarBarra() {
+        barra.style.display = "none";
+      }
+
+      
+      // #region REGIONES
+
+      var tarjetaDanza1 = document.querySelector(".stickynote1")
+      function mostrartDanza1(){
+        tarjetaDanza1.style.display = "flex"
+      }
+      function ocultarTarjeta(){
+        tarjetaDanza1.style.display = "none"
+      }
+      
+      var tarjetaDanza2 = document.querySelector(".stickynote2")
+      function mostrarDanza2(){
+        tarjetaDanza2.style.display = "flex"
+      }
+      function ocultarTarjeta2(){
+        tarjetaDanza2.style.display = "none"
+      }
+      
+      
+      var tarjetaDanza3 = document.querySelector(".stickynote3")
+      function mostrartDanza3(){
+        tarjetaDanza3.style.display = "flex"
+      }
+      function ocultarTarjeta3(){
+        tarjetaDanza3.style.display = "none"
+      }
+      
+      
+      
+      var tarjetaDanza4 = document.querySelector(".stickynote4")
+      function mostrartDanza4(){
+        tarjetaDanza4.style.display = "flex"
+      }
+      function ocultarTarjeta4(){
+        tarjetaDanza4.style.display = "none"
+      }
+
+      
+      
+      var tarjetaDanza5 = document.querySelector(".stickynote5")
+      function mostrartDanza5(){
+        tarjetaDanza5.style.display = "flex"
+      }
+      function ocultarTarjeta5(){
+        tarjetaDanza5.style.display = "none"
+      }
+      
+      var tarjetaDanza6 = document.querySelector(".stickynote6")
+      function mostrartDanza6(){
+        tarjetaDanza6.style.display = "flex"
+      }
+      function ocultarTarjeta6(){
+        tarjetaDanza6.style.display = "none"
+      }
+
+
+
+
+      
